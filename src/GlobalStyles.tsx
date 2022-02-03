61d98ca06de59b28fd6fcbd6f0bd9ff39f651ba9
sepehrdad <sepehrdad_kashiri@yahoo.com>
2022-02-04 02:44:19 +0330

    added font

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,11 +1,17 @@
 import { createGlobalStyle } from "styled-components";
+import samim from "./assets/Samim.woff2";
 
 const GlobalStyles = createGlobalStyle`
+@font-face{
+    font-family:'samim';
+    src:url(${samim})
+}
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         direction: rtl;
+        font-family:'samim';
     }
     body{
         background-color: ${({ theme }) => theme.mainColor};
